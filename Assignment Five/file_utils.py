b88eafae7d30679f7938cc03e1695cf3d15f9e19
Taha Saif <tahasaifsurhio454@gmail.com>
2025-05-16 09:53:48 +0500

    updated code

--- Assignment Five/file_utils.py
+++ Assignment Five/file_utils.py
@@ -3,26 +3,26 @@ class ATM:
         self.__balance = 5000
         self.__pin = 1234
 
-    def check_pin(self, input_pin:int):
+    def check_pin(self,input_pin:int):
         return input_pin == self.__pin
-
-    def check_balance(self, input_pin):
+    
+    def check_balance(self,input_pin):
         if self.check_pin(input_pin):
             print(f"Your Current Balance is: {self.__balance} rupees in your account.")
         else:
             print("Invalid PIN!")
 
-    def deposit(self, amount, input_pin):
+    def deposit(self,amount:int,input_pin:int):
         if not self.check_pin(input_pin):
             print("Invalid PIN!")
         elif amount > 0:
             self.__balance += amount
-            print(f"Deposited {amount} rupees in your account.")
-            print(f"New balance is {self.__balance} rupees")
+            print(f"You have Deposited {amount} rupees in your account.")
+            print(f"Your New balance is {self.__balance} rupees")
         else:
             print("Amount must be greater than 0")
 
-    def withdraw(self, amount, input_pin):
+    def withdraw(self,amount:int,input_pin:int):
         if not self.check_pin(input_pin):
             print("Invalid PIN!")
         elif amount > self.__balance:
@@ -31,8 +31,8 @@ class ATM:
             print("Invalid amount")
         else:
             self.__balance -= amount
-            print(f"Withdrawn {amount}. rupees from your account.")  
-            print(f"New balance is {self.__balance} rupees")    
-
+            print(f"You have Withdrawn {amount}. rupees from your account.")  
+            print(f"Your New balance is {self.__balance} rupees")
+    
     def quit(self):
         print("Exiting the ATM Machine CLI!")
